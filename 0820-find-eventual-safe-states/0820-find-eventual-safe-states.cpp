9a1fa7139eff6b1a879b3ff7fd1da6571c4c6b52
hardikmakkar07 <121104903+hardikmakkar07@users.noreply.github.com>
2025-04-11 03:51:09 +0530

    Time: 56 ms (26.84%), Space: 66.2 MB (48.43%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,39 +1,49 @@
 class Solution 
 {
 public:
-    bool dfs(int i, vector<bool> &vis, vector<bool> &pathVis, vector<vector<int>> &graph)
-    {
-        vis[i] = true;
-        pathVis[i] = true;
-        for(int j = 0; j<graph[i].size(); j++)
-        {
-            if(!vis[graph[i][j]])
-            {
-                if(dfs(graph[i][j],vis,pathVis,graph))
-                    return true;
-            }
-            else if(pathVis[graph[i][j]])
-                return true;
-        }
-        pathVis[i] = false;
-        return false;
-    }
+    // bool dfs(int i, vector<bool> &vis, vector<bool> &pathVis, vector<vector<int>> &graph)
+    // {
+        
+    // }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
     {
         int n = graph.size();
-        vector<bool> vis(n,false), pathVis(n,false);
-
+        vector<vector<int>> adj(n,vector<int>());
         for(int i = 0; i<n; i++)
         {
-            if(!vis[i])
-                dfs(i,vis,pathVis,graph);
+            for(int j = 0; j<graph[i].size(); j++)
+            {
+                adj[graph[i][j]].push_back(i);
+            }
+        }
+        vector<int> indegree(n,0);
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<adj[i].size(); j++)
+            {
+                indegree[adj[i][j]]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i<n; i++)
+        {
+            if(!indegree[i])
+                q.push(i);
         }
         vector<int> ans;
-        for(int i = 0; i<n; i++)
+        while(!q.empty())
         {
-            if(!pathVis[i])
-                ans.push_back(i);
+            int x = q.front();
+            ans.push_back(x);
+            q.pop();
+            for(int j = 0; j<adj[x].size(); j++)
+            {
+                indegree[adj[x][j]]--;
+                if(indegree[adj[x][j]]==0)
+                    q.push(adj[x][j]);
+            }
         }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
