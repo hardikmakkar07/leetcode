ddbc5970c532b4adaae9aa666d1b55aac927af91
hardikmakkar07 <121104903+hardikmakkar07@users.noreply.github.com>
2025-05-26 18:36:03 +0530

    Time: 164 ms (6.36%), Space: 47.8 MB (32.52%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) 
+    {
+        vector<vector<pair<int,int>>> adj(n+1,vector<pair<int,int>>());
+        for(auto it: times)
+            adj[it[0]].push_back({it[1],it[2]});
+        for(int i = 0; i<n+1; i++)
+        {
+            cout<<i<<endl;
+            for(int j = 0; j<adj[i].size(); j++)
+                cout<<adj[i][j].first<<" "<<adj[i][j].second<<endl;
+        }
+        vector<int> dist(n+1,1e9);
+        dist[k] = 0;
+        queue<pair<int,int>> q;
+        q.push({0,k});
+        while(!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+            int node = it.second;
+            int d = it.first;
+            for(auto x: adj[node])
+            {
+                int newNode = x.first;
+                int len = x.second;
+                if(dist[newNode]>len+d)
+                {
+                    dist[newNode] = len+d;
+                    q.push({dist[newNode],newNode});
+                }
+            }
+        }
+        int x = 0;
+        for(int i = 1; i<n+1; i++)
+        {
+            if(dist[i]>x)
+                x = dist[i];
+        }
+        if(x==1e9) return -1;
+        return x;
+    }
+};
