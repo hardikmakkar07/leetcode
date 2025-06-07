fa6d25eb2da52f9ab0dd1f9c5672f86fe00cf043
hardikmakkar07 <121104903+hardikmakkar07@users.noreply.github.com>
2025-06-07 09:20:21 +0530

    Time: 1 ms (92.95%), Space: 12.5 MB (96.81%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -3,12 +3,15 @@ public:
     bool isValid(int adjr, int adjc, int n, int m) {
         return adjr >= 0 && adjr < n && adjc >= 0 && adjc < m;
     }
-    bool check(vector<vector<int>> &grid, int mid, int n)
+    
+    int swimInWater(vector<vector<int>>& grid) 
     {
+        int n = grid.size();
+        int l = 0, r = n*n-1;
+
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
         vector<bool> vis(n*n,false);
-        if(grid[0][0]<=mid)
-            pq.push({grid[0][0],{0,0}});
+        pq.push({grid[0][0],{0,0}});
         int dr[] = { -1, 0, 1, 0};
         int dc[] = {0, 1, 0, -1};
         vis[grid[0][0]] = true;
@@ -19,12 +22,12 @@ public:
             pq.pop();
             
             if(row==n-1 && col==n-1)
-                return true;
+                return maxEle;
             for(int ind = 0; ind < 4; ind++) 
             {
                 int adjr = row+dr[ind];
                 int adjc = col+dc[ind];
-                if(isValid(adjr,adjc,n,n) && grid[adjr][adjc]<=mid && !vis[grid[adjr][adjc]])
+                if(isValid(adjr,adjc,n,n) && !vis[grid[adjr][adjc]])
                 {
                     int ma = max(maxEle,grid[adjr][adjc]);
                     vis[grid[adjr][adjc]] = true;
@@ -32,25 +35,6 @@ public:
                 }
             }
         }
-        return false;
-    }
-    int swimInWater(vector<vector<int>>& grid) 
-    {
-        int n = grid.size();
-        int l = 0, r = n*n-1;
-
-        int ans = r;
-        while(l<=r)
-        {
-            int mid = l+(r-l)/2;
-            if(check(grid,mid,n))
-            {
-                ans = mid;
-                r = mid-1;
-            }
-            else
-                l = mid+1;
-        }
-        return ans;
+        return n*n-1;
     }
 };
