55b2ac67788ffbe3d72db07a55ca2f7f7b9b7118
hardikmakkar07 <121104903+hardikmakkar07@users.noreply.github.com>
2025-06-11 11:19:47 +0530

    Time: 0 ms (100%), Space: 10.7 MB (49.09%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    void answer(int ind, int n, vector<int> &ds, vector<int> &nums, vector<vector<int>> &ans, vector<bool> &pick)
+    void answer(int ind, int n, vector<int> &nums, vector<vector<int>> &ans)
     {
         if(ind==n)
         {
-            ans.push_back(ds);
+            ans.push_back(nums);
             return;
         }
-        for(int i = 0; i<n; i++)
+        for(int i = ind; i<n; i++)
         {
-            if(!pick[i])
-            {
-                ds.push_back(nums[i]);
-                pick[i] = true;
-                answer(ind+1,n,ds,nums,ans,pick);
-                ds.pop_back();
-                pick[i] = false;
-            }
+            swap(nums[i],nums[ind]);
+            answer(ind+1,n,nums,ans);
+            swap(nums[i],nums[ind]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) 
     {
         vector<vector<int>> ans;
         int n = nums.size();
-        vector<int> ds;
-        vector<bool> pick(n,false);
-        answer(0,n,ds,nums,ans,pick);
+        answer(0,n,nums,ans);
         return ans;
     }
 };
